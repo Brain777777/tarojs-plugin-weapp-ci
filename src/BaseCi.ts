396c46dbeab19b022da6e4889b2591a49680f50c
Brain777777 <35163869+Brain777777@users.noreply.github.com>
2022-03-24 18:37:10 +0800

    first commit

---
+++ src/BaseCi.ts
@@ -0,0 +1,82 @@
+import { IPluginContext } from "@tarojs/service";
+
+export type ProjectType =
+  | "miniProgram"
+  | "miniGame"
+  | "miniProgramPlugin"
+  | "miniGamePlugin";
+
+export type QrcodeType = "image" | "base64" | "terminal";
+
+/** 微信小程序配置 */
+export interface WeappConfig {
+  /** 小程序/小游戏项目的 appid */
+  appid: string;
+  /** 私钥，在获取项目属性和上传时用于鉴权使用(必填) */
+  privateKeyPath: string;
+  /** 微信开发者工具安装路径 */
+  devToolsInstallPath?: string;
+  /** 上传的小程序的路径（默认 outputPath ） */
+  projectPath?: string;
+  /** 类型，默认miniProgram 小程序 */
+  type?: ProjectType;
+  /** 上传需要排除的目录 */
+  ignores?: Array<string>;
+}
+
+export interface CIOptions {
+  /** 微信小程序CI项目配置, */
+  weapp?: WeappConfig;
+  /** 发布版本号 */
+  version: string;
+  /** 版本发布描述 */
+  desc: string;
+  /** 指定使用哪一个 ci 机器人，可选值：1 ~ 30 */
+  robot?: number;
+  /** 返回二维码文件的格式 "image" 或 "base64"， 默认值 "terminal" 供调试用 */
+  qrcodeFormat?: QrcodeType;
+  /** 二维码文件保存路径 */
+  qrcodeOutputDest?: string;
+  /** 预览页面路径 */
+  pagePath?: string;
+  /** 预览页面路径启动参数 */
+  searchQuery?: string;
+  /** 默认值 1011，具体含义见场景值列表: https://developers.weixin.qq.com/miniprogram/dev/reference/scene-list.html */
+  scene?: number;
+}
+
+export default abstract class BaseCI {
+  /** taro 插件上下文 */
+  protected ctx: IPluginContext;
+
+  /** 传入的插件选项 */
+  protected pluginOpts: CIOptions;
+
+  /** 当前要发布的版本号 */
+  protected version: string;
+
+  /** 当前发布内容的描述 */
+  protected desc: string;
+
+  constructor(ctx: IPluginContext, pluginOpts: CIOptions) {
+    this.ctx = ctx;
+    this.pluginOpts = pluginOpts;
+    this.version = pluginOpts.version || "1.0.0";
+    this.desc =
+      pluginOpts.desc || `CI构建自动构建于${new Date().toLocaleTimeString()}`;
+
+    this._init();
+  }
+
+  /** 初始化函数，会被构造函数调用 */
+  protected abstract _init(): void;
+
+  /** 打开小程序项目 */
+  abstract open();
+
+  /** 上传小程序 */
+  abstract upload();
+
+  /** 预览小程序 */
+  abstract preview();
+}
---
+++ src/index.ts
@@ -0,0 +1,73 @@
+import { IPluginContext } from "@tarojs/service";
+import * as minimist from "minimist";
+import { CIOptions } from "./BaseCi";
+import WeappCI from "./WeappCI";
+
+export { CIOptions } from "./BaseCi";
+export default (ctx: IPluginContext, pluginOpts: CIOptions) => {
+  const onBuildDone = ctx.onBuildComplete || ctx.onBuildFinish;
+
+  ctx.addPluginOptsSchema((joi) => {
+    return joi
+      .object()
+      .keys({
+        /** 微信小程序上传配置 */
+        weapp: joi.object({
+          appid: joi.string().required(),
+          privateKeyPath: joi.string().required(),
+          projectPath: joi.string(),
+          type: joi
+            .string()
+            .valid(
+              "miniProgram",
+              "miniProgramPlugin",
+              "miniGame",
+              "miniGamePlugin"
+            ),
+          ignores: joi.array().items(joi.string().required()),
+        }),
+        version: joi.string(),
+        desc: joi.string(),
+        robot: joi.number(),
+        qrcodeFormat: joi.string().valid("image",""),
+        qrcodeOutputDest: joi.string(),
+        pagePath: joi.string(),
+        searchQuery: joi.string(),
+        scene: joi.number(),
+      })
+      .required();
+  });
+
+  onBuildDone(async () => {
+    const args = minimist(process.argv.slice(2), {
+      boolean: ["open", "upload", "preview"],
+    });
+    const { printLog, processTypeEnum } = ctx.helper;
+    const platform = ctx.runOpts.options.platform;
+    let ci;
+    switch (platform) {
+      case "weapp":
+        ci = new WeappCI(ctx, pluginOpts);
+        break;
+      default:
+        break;
+    }
+    if (!ci) {
+      printLog(processTypeEnum.WARNING, `"插件暂时不支持 "${platform}" 平台`);
+      return;
+    }
+    switch (true) {
+      case args.open:
+        ci.open();
+        break;
+      case args.upload:
+        ci.upload();
+        break;
+      case args.preview:
+        ci.preview();
+        break;
+      default:
+        break;
+    }
+  });
+};
